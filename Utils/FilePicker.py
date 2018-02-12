c8e38fccfe803825f491131846f33f558d6f34a6
Owain van Brakel <owain.vanbrakel@gmail.com>
2018-02-12 18:06:13 +0100

    Basic logging

--- Utils/FilePicker.py
+++ Utils/FilePicker.py
@@ -1,18 +1,24 @@
 import os
 
 from asciimatics.event import KeyboardEvent
-from asciimatics.widgets import Frame, Layout, FileBrowser, Widget, Label, Text, Divider
+from asciimatics.widgets import Frame, Layout, FileBrowser, Widget, Label, \
+    Text, Divider
 from asciimatics.scene import Scene
 from asciimatics.screen import Screen
 from asciimatics.exceptions import ResizeScreenError, StopApplication
+
+from Utils.Logging.Logging import Logging
 
 global_store = None
 
 
 class FilepickerFrame(Frame):
     def __init__(self, screen, store):
+        self.logger = Logging(self.__class__.__name__).logger
+
         super(FilepickerFrame, self).__init__(
-            screen, screen.height, screen.width, has_border=False, name="Filepicker")
+            screen, screen.height, screen.width, has_border=False,
+            name='Filepicker')
 
         self.store = store
 
@@ -23,31 +29,34 @@ class FilepickerFrame(Frame):
         # Now populate it with the widgets we want to use.
         self._details = Text()
         self._details.disabled = True
-        self._details.custom_colour = "field"
+        self._details.custom_colour = 'field'
         self._list = FileBrowser(Widget.FILL_FRAME,
-                                 os.path.abspath("."),
-                                 name="mc_list",
+                                 os.path.abspath('.'),
+                                 name='mc_list',
                                  on_select=self.selected)
-        layout.add_widget(Label("Image file picker"))
+        layout.add_widget(Label('Image file picker'))
         layout.add_widget(Divider())
         layout.add_widget(self._list)
         layout.add_widget(Divider())
         layout.add_widget(self._details)
-        layout.add_widget(Label("Press Enter to select or `q` to return."))
+        layout.add_widget(Label('Press Enter to select or `q` to return.'))
 
         # Prepare the Frame for use.
         self.fix()
+        
+        self.logger.debug('File picker opened')
 
     def selected(self):
         # Just confirm whenever the user actually selects something.
         self.store.dispatch({'type': 'set_image', 'image': self._list.value})
-        raise StopApplication("Image selected")
+        self.logger.info('Image selected: {}'.format(self._list.value))
+        raise StopApplication('Image selected')
 
     def process_event(self, event):
         # Do the key handling for this Frame.
         if isinstance(event, KeyboardEvent):
-            if event.key_code in [ord('q'), ord('Q'), Screen.ctrl("c")]:
-                raise StopApplication("User quit")
+            if event.key_code in [ord('q'), ord('Q'), Screen.ctrl('c')]:
+                raise StopApplication('User quit')
 
         # Now pass on to lower levels for normal handling of the event.
         return super(FilepickerFrame, self).process_event(event)
@@ -55,7 +64,8 @@ class FilepickerFrame(Frame):
 
 def filepicker(screen, old_scene):
     global global_store
-    screen.play([Scene([FilepickerFrame(screen, global_store)], -1)], stop_on_resize=True, start_scene=old_scene)
+    screen.play([Scene([FilepickerFrame(screen, global_store)], -1)],
+                stop_on_resize=True, start_scene=old_scene)
 
 
 def filepicker_main(store):
@@ -65,7 +75,8 @@ def filepicker_main(store):
     last_scene = None
     while True:
         try:
-            Screen.wrapper(filepicker, catch_interrupt=False, arguments=[last_scene])
+            Screen.wrapper(filepicker, catch_interrupt=False,
+                           arguments=[last_scene])
             break
         except ResizeScreenError as e:
             last_scene = e.scene
--- Utils/Menu.py
+++ Utils/Menu.py
@@ -3,9 +3,13 @@ import sys
 
 from curses import panel
 
+from Utils.Logging.Logging import Logging
+
 
 class Menu(object):
     def __init__(self, items, stdscreen, sub: bool = True):
+        self.logger = Logging(self.__class__.__name__).logger
+
         self.window = stdscreen.subwin(0, 0)
         self.window.keypad(1)
         self.panel = panel.new_panel(self.window)
@@ -14,14 +18,17 @@ class Menu(object):
 
         self.position = 0
         self.items = items
+        self.sub = sub
         if sub:
             self.items.append(('Return', 'return'))
+            self.logger.debug('Submenu instantiated')
+        else:
+            self.logger.debug('Menu instantiated')
 
         # Start colors in curses
         curses.start_color()
-        curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
-        curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-        curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
+        curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
     def navigate(self, n):
         self.position += n
@@ -39,29 +46,33 @@ class Menu(object):
             height, width = self.window.getmaxyx()
 
             # Declaration of strings
-            title = "IPFIT5"[:width - 1]
-            subtitle = "Owain van Brakel | Kasper van den Berg | Virgil Bron"[:width - 1]
-            statusbarstr = "Press 'q' to exit"
+            title = 'IPFIT5'[:width - 1]
+            subtitle = 'Owain van Brakel | Kasper van den Berg | Virgil Bron'[
+                       :width - 1]
+            statusbarstr = 'Press \'q\' to exit'
 
             # Centering calculations
-            start_x_title = int((width // 2) - (len(title) // 2) - len(title) % 2)
-            start_x_subtitle = int((width // 2) - (len(subtitle) // 2) - len(subtitle) % 2)
+            start_x_title = int(
+                (width // 2) - (len(title) // 2) - len(title) % 2)
+            start_x_subtitle = int(
+                (width // 2) - (len(subtitle) // 2) - len(subtitle) % 2)
 
             # Render status bar
-            self.window.attron(curses.color_pair(3))
+            self.window.attron(curses.color_pair(2))
             self.window.addstr(height - 1, 0, statusbarstr)
-            self.window.addstr(height - 1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
-            self.window.attroff(curses.color_pair(3))
+            self.window.addstr(height - 1, len(statusbarstr),
+                               ' ' * (width - len(statusbarstr) - 1))
+            self.window.attroff(curses.color_pair(2))
 
             # Turning on attributes for title
-            self.window.attron(curses.color_pair(2))
+            self.window.attron(curses.color_pair(1))
             self.window.attron(curses.A_BOLD)
 
             # Rendering title
             self.window.addstr(0, start_x_title, title)
 
             # Turning off attributes for title
-            self.window.attroff(curses.color_pair(2))
+            self.window.attroff(curses.color_pair(1))
             self.window.attroff(curses.A_BOLD)
 
             # Print rest of text
@@ -91,6 +102,7 @@ class Menu(object):
 
             if key in [curses.KEY_ENTER, ord('\n')]:
                 if (self.items[self.position][1]) == 'return':
+                    self.logger.debug('Submenu exited')
                     break
                 else:
                     self.items[self.position][1]()
@@ -102,6 +114,7 @@ class Menu(object):
                 self.navigate(1)
 
             if key == ord('q') or key == ord('Q'):
+                self.logger.info('Application exited')
                 sys.exit('User exited')
 
         self.window.clear()
