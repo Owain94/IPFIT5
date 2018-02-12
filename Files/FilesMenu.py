a32614de94c82d475254bd82c892fdf22e8f2c04
Owain van Brakel <owain.vanbrakel@gmail.com>
2018-02-12 13:24:00 +0100

    Menu

---
+++ Files/FilesMenu.py
@@ -0,0 +1,11 @@
+import curses
+
+
+class FilesMenu(object):
+    @staticmethod
+    def menu():
+        menu_items = [
+            ('Placeholder', curses.beep)
+        ]
+
+        return menu_items
---
+++ Store.py
@@ -0,0 +1,16 @@
+import pydux
+
+
+class Store:
+    def __init__(self):
+        self.image_store = pydux.create_store(self.image)
+
+    @staticmethod
+    def image(state, action):
+        if state is None:
+            state = 'initial'
+        if action is None:
+            return state
+        elif action['type'] == 'set_image':
+            state = action['image']
+        return state
