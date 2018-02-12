f6dca63af300b890966b5b92537f5a02d76a382b
Owain van Brakel <owain.vanbrakel@gmail.com>
2018-02-12 13:45:24 +0100

    Test TravisCI

---
+++ pytest.py
@@ -0,0 +1,18 @@
+import pytest
+
+from Store import Store
+
+
+def test_image_store_case():
+    store = Store()
+    assert store.image_store.get_state() == 'initial'
+    store.image_store.dispatch({'type': 'set_image', 'image': 'test'})
+    assert store.image_store.get_state() == 'test'
+
+
+def main():
+    test_image_store_case()
+
+
+if __name__ == '__main__':
+    main()
