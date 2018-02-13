c34a1ea649e9b0d21d043549925840f2180cd5c5
DutchGhost <kasper199914@gmail.com>
2018-02-13 12:23:23 +0100

    IDK, nog ff kijken met hoe we die bestandjes uit de image gaan openen ofzo?

---
+++ IP/IP.py
@@ -0,0 +1,69 @@
+import os
+import mmap
+import hashlib
+import contextlib
+
+from itertools import chain
+from multiprocessing import Pool, cpu_count
+import time
+
+BLOCKSIZE = 65536
+
+def flatten(iterable):
+    iterator, sentinel, stack = iter(iterable), object(), []
+    while True:
+        value = next(iterator, sentinel)
+        if value is sentinel:
+            if not stack:
+                break
+            iterator = stack.pop()
+        elif isinstance(value, str):
+            yield value
+        else:
+            try:
+                new_iterator = iter(value)
+            except TypeError:
+                yield value
+            else:
+                stack.append(iterator)
+                iterator = new_iterator
+def getSize(filename):
+    st = os.stat(filename)
+    return st.st_size
+
+def hash_file(file):
+    hasher = hashlib.sha256()
+    s = getSize(file)
+    if s <= 0 or s > 1000000000:
+        return ""
+    with open(file, 'rb') as f:
+
+        with contextlib.closing(mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)) as m:
+            buf = m.read(BLOCKSIZE)
+            while len(buf) > 0:
+                hasher.update(buf)
+                buf = m.read(BLOCKSIZE)
+
+    return hasher.hexdigest()
+
+def hash_tree(files):
+    P = Pool(cpu_count())
+
+    hashed = P.map(hash_file, files)
+
+    return hashed
+
+def walk_dir():
+    for root, dirs, files in os.walk("C:/Users/Kasper/Documents", topdown=False):
+        for name in files:
+            yield os.path.join(root, name)
+    print("Done yielding things")
+if __name__ == '__main__':
+    n = 0
+    for f in walk_dir():
+        n += 1
+    print(n)
+    s = time.time()
+    t = hash_tree([f for f in walk_dir()])
+
+    print(time.time() - s)
