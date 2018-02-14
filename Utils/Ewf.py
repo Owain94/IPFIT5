4a81d1bc1977d2a8c207dee763ba977a9a5953e9
Owain van Brakel <owain.vanbrakel@gmail.com>
2018-02-14 18:28:28 +0100

    Support dd images

--- Utils/Ewf.py
+++ Utils/Ewf.py
@@ -2,6 +2,8 @@ import pyewf
 import pytsk3
 
 from os import sep
+from sys import setrecursionlimit
+from pathlib import Path as PathlibPath
 
 from Utils.Logging.Logging import Logging
 
@@ -9,13 +11,22 @@ from Utils.Logging.Logging import Logging
 class Ewf(pytsk3.Img_Info):
     def __init__(self, store):
         self.logger = Logging(self.__class__.__name__).logger
-        filenames = pyewf.glob(store.get_state())
-        self.ewf_handle = pyewf.handle()
-        self.ewf_handle.open(filenames)
-        self.logger.debug('EWF handle opened')
-        self.logger.info('{} loaded with EWF'.format(
-            store.get_state().split(sep)[-1]))
-        super(Ewf, self).__init__(url='', type=pytsk3.TSK_IMG_TYPE_EXTERNAL)
+        self.store = store
+
+        setrecursionlimit(100000)
+
+        self.image_handle = None
+
+        self.ext = PathlibPath(store.get_state()).suffix.lower()[1:]
+        self.logger.debug('Extension: ' + self.ext)
+
+        if self.ext == 'e01' or self.ext == 's01' or self.ext == 'ex01' or self.ext == 'l01' or self.ext == 'lx01':
+            self.ewf_handle = pyewf.handle()
+            self.ewf_handle.open(pyewf.glob(store.get_state()))
+            self.logger.debug('EWF handle opened')
+            self.logger.info('{} loaded with EWF'.format(
+                store.get_state().split(sep)[-1]))
+            super(Ewf, self).__init__(url='', type=pytsk3.TSK_IMG_TYPE_EXTERNAL)
 
     def close(self):
         self.logger.debug('EWF handle closed')
@@ -29,8 +40,12 @@ class Ewf(pytsk3.Img_Info):
         return self.ewf_handle.get_media_size()
 
     def info(self):
-        volume = pytsk3.Volume_Info(self)
-        self.close()
+        if self.ext == 'e01' or self.ext == 's01' or self.ext == 'ex01' or self.ext == 'l01' or self.ext == 'lx01':
+            volume = pytsk3.Volume_Info(self)
+            self.close()
+        else:
+            self.image_handle = pytsk3.Img_Info(url=self.store.get_state())
+            volume = pytsk3.Volume_Info(self.image_handle)
         return volume
 
 
