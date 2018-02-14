7fce773ab8b235fa413c3c4eb2d0f75a3cf1f234
Owain van Brakel <owain.vanbrakel@gmail.com>
2018-02-14 19:44:53 +0100

    File recursion

--- Utils/Ewf.py
+++ Utils/Ewf.py
@@ -1,8 +1,11 @@
 import pyewf
 import pytsk3
 
+import csv
+from datetime import datetime
+
 from os import sep
-from sys import setrecursionlimit
+from sys import setrecursionlimit, exc_info, exit
 from pathlib import Path as PathlibPath
 
 from Utils.Logging.Logging import Logging
@@ -21,13 +24,15 @@ class Ewf(pytsk3.Img_Info):
         self.block_size = 0
         self.logger.debug('Extension: ' + self.ext)
 
-        if self.ext == 'e01' or self.ext == 's01' or self.ext == 'ex01' or self.ext == 'l01' or self.ext == 'lx01':
+        if self.ext == 'e01' or self.ext == 's01' or self.ext == 'ex01' or \
+                self.ext == 'l01' or self.ext == 'lx01':
             self.ewf_handle = pyewf.handle()
             self.ewf_handle.open(pyewf.glob(store.get_state()))
             self.logger.debug('EWF handle opened')
             self.logger.info('{} loaded with EWF'.format(
                 store.get_state().split(sep)[-1]))
-            super(Ewf, self).__init__(url='', type=pytsk3.TSK_IMG_TYPE_EXTERNAL)
+            super(Ewf, self).__init__(url='',
+                                      type=pytsk3.TSK_IMG_TYPE_EXTERNAL)
 
     def close(self):
         self.logger.debug('EWF handle closed')
@@ -41,7 +46,8 @@ class Ewf(pytsk3.Img_Info):
         return self.ewf_handle.get_media_size()
 
     def info(self):
-        if self.ext == 'e01' or self.ext == 's01' or self.ext == 'ex01' or self.ext == 'l01' or self.ext == 'lx01':
+        if self.ext == 'e01' or self.ext == 's01' or self.ext == 'ex01' or \
+                self.ext == 'l01' or self.ext == 'lx01':
             volume = pytsk3.Volume_Info(self)
             self.close()
         else:
@@ -50,6 +56,107 @@ class Ewf(pytsk3.Img_Info):
 
         self.block_size = volume.info.block_size
         return volume
+
+    def files(self):
+        vol = self.info()
+        if self.ext == 'e01' or self.ext == 's01' or self.ext == 'ex01' or \
+                self.ext == 'l01' or self.ext == 'lx01':
+            img = self
+        else:
+            img = self.image_handle
+        # print("[+] Recursing through files..")
+        recursed_data = []
+        fs = None
+        # Open FS and Recurse
+        if vol is not None:
+            for part in vol:
+                if part.len > 2048 and "Unallocated" not in part.desc.decode(
+                        'UTF-8') and "Extended" not in part.desc.decode(
+                        'UTF-8') and "Primary Table" not in part.desc.decode(
+                        'UTF-8'):
+                    try:
+                        fs = pytsk3.FS_Info(
+                            img, offset=part.start * vol.info.block_size)
+                    except IOError:
+                        _, e, _ = exc_info()
+                        # print("[-] Unable to open FS:\n {}".format(e))
+                    root = fs.open_dir(path="/")
+                    data = self.recurse_files(part.addr, fs, root, [], [],
+                                              [""])
+                    recursed_data.append(data)
+
+        else:
+            try:
+                fs = pytsk3.FS_Info(img)
+            except IOError:
+                _, e, _ = exc_info()
+                # print("[-] Unable to open FS:\n {}".format(e))
+            root = fs.open_dir(path="/")
+            data = self.recurse_files(1, fs, root, [], [], [""])
+            recursed_data.append(data)
+
+        return recursed_data
+
+    def recurse_files(self, part, fs, root_dir, dirs, data, parent):
+        # print('Recurse')
+        dirs.append(root_dir.info.fs_file.meta.addr)
+        for fs_object in root_dir:
+            # Skip ".", ".." or directory entries without a name.
+            if not hasattr(fs_object, "info") \
+                    or not hasattr(fs_object.info, "name") or not hasattr(
+                    fs_object.info.name, "name") or \
+                    fs_object.info.name.name in [".", ".."]:
+                continue
+            try:
+                file_name = fs_object.info.name.name.decode('UTF-8')
+                file_path = "{}/{}".format(
+                    "/".join(parent),
+                    fs_object.info.name.name.decode('UTF-8'))
+                try:
+                    if fs_object.info.meta.type == \
+                            pytsk3.TSK_FS_META_TYPE_DIR:
+                        f_type = "DIR"
+                        file_ext = ""
+                    else:
+                        f_type = "FILE"
+                        if "." in file_name:
+                            file_ext = PathlibPath(
+                                file_name.decode('UTF-8')).suffix.lower()[1:]
+                        else:
+                            file_ext = ""
+                except AttributeError:
+                    continue
+
+                size = fs_object.info.meta.size
+                create = self.convert_time(fs_object.info.meta.crtime)
+                change = self.convert_time(fs_object.info.meta.ctime)
+                modify = self.convert_time(fs_object.info.meta.mtime)
+                data.append(
+                    ["PARTITION {}".format(part), file_name, file_ext, f_type,
+                     create, change, modify, size, file_path])
+
+                if f_type == "DIR":
+                    parent.append(fs_object.info.name.name.decode('UTF-8'))
+                    sub_directory = fs_object.as_directory()
+                    inode = fs_object.info.meta.addr
+
+                    # This ensures that we don't recurse into a directory
+                    # above the current level and thus avoid circular loops.
+                    if inode not in dirs:
+                        self.recurse_files(part, fs, sub_directory, dirs,
+                                           data, parent)
+                    parent.pop(-1)
+
+            except IOError:
+                pass
+        dirs.pop(-1)
+        return data
+
+    @staticmethod
+    def convert_time(ts):
+        if str(ts) == "0":
+            return ""
+        return datetime.utcfromtimestamp(ts)
 
 
 class EwfInfoMenu(object):
@@ -61,7 +168,9 @@ class EwfInfoMenu(object):
         ewf = Ewf(store)
         volume = ewf.info()
 
-        menu_items = [('Amount of partitions: {}'.format(volume.info.part_count), ''), ('', '')]
+        menu_items = [
+            ('Amount of partitions: {}'.format(volume.info.part_count), ''),
+            ('', '')]
 
         for part in volume:
             menu_items.append(('Partition address: {}'.format(part.addr), ''))
