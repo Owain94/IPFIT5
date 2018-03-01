b89b18a606d83cf3ca940c218579a7b7015a722e
Miesvanderlippe <miesvanderlippe@gmail.com>
2018-03-01 14:44:32 +0100

    Added user credentials to store

--- Utils/Store.py
+++ Utils/Store.py
@@ -5,6 +5,22 @@ from Utils.Singleton import Singleton
 class Store(metaclass=Singleton):
     def __init__(self):
         self.image_store = pydux.create_store(self.image)
+        self.credential_store = pydux.create_store(self.credential)
+
+    @staticmethod
+    def credential(state: str, action: [str, str]) -> str:
+        if state is None:
+            state = {
+                'name': '',
+                'location': ''
+            }
+        if action is None:
+            return state
+        elif action['type'] == 'set_location':
+            state['location'] = action['location']
+        elif action['type'] == 'set_name':
+            state['location'] = action['name']
+        return state
 
     @staticmethod
     def image(state, action):
