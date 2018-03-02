0793b98b8ef6b89be6d1f6c876ebc52d2aed9ad9
Miesvanderlippe <miesvanderlippe@gmail.com>
2018-03-02 12:46:47 +0100

    Added saving to disk to store
    Conflicts:
    	.gitignore
    	Utils/Store.py

--- Utils/Store.py
+++ Utils/Store.py
@@ -1,5 +1,7 @@
 import pydux
 from Utils.Singleton import Singleton
+from pathlib import Path
+from os import sep
 
 
 class Store(metaclass=Singleton):
@@ -8,16 +10,53 @@ class Store(metaclass=Singleton):
         self.credential_store = pydux.create_store(self.credential)
 
     @staticmethod
-    def credential(state: str, action: [str, str]) -> str:
-        if state is None:
-            state = {
-                'name': '',
-                'location': '',
-                'case': ''
+    def get_config_save_path(type: str) -> Path:
+        # Make config folder
+        config_path = Path(__file__).parent.parent.joinpath('Configs')
+        Path.mkdir(Path(config_path), exist_ok=True)
+
+        # Make config file
+        config_file_path = Path(config_path.joinpath("{0}.cfg".format(type)))
+        Path.touch(config_file_path, exist_ok=True)
+
+        return config_file_path
+
+    @staticmethod
+    def write_config_to_disk(path: Path, config: dict) -> None:
+        with open(path, 'w') as file:
+            file.writelines(
+                sep.join([str(x) + ":" + str(y) for x, y in config.items()])
+            )
+
+    @staticmethod
+    def read_config_from_disk(path: Path, defaults: dict) -> dict:
+        with open(path, 'r') as file:
+            lines = {
+                split_line[0]: ":".join(split_line[1::]) for split_line in
+                [line.strip().split(':') for line in file.readlines()]
             }
+
+        for key, value in defaults.items():
+            if key in lines:
+                defaults[key] = lines[key]
+
+        return defaults
+
+    @staticmethod
+    def credential(state: str, action: [str, str]) -> dict:
+        if state is None:
+            state = Store.read_config_from_disk(
+                Store.get_config_save_path('credentials'),
+                {
+                    'name': '',
+                    'location': '',
+                    'case': ''
+                }
+            )
+
         if action is None:
             return state
-        elif action == 'set_credentials':
+        elif action['type'] == 'set_credentials':
             state = {
                 'name': action['credentials']['name'],
                 'location': action['credentials']['location'],
@@ -29,6 +68,11 @@ class Store(metaclass=Singleton):
             state['location'] = action['name']
         elif action['type'] == 'set_case':
             state['case'] = action['case']
+        elif action['type'] == 'safe_to_disk':
+            Store.write_config_to_disk(
+                Store.get_config_save_path("credentials"),
+                state
+            )
         return state
 
     @staticmethod
@@ -40,3 +84,11 @@ class Store(metaclass=Singleton):
         elif action['type'] == 'set_image':
             state = action['image']
         return state
+
+
+if __name__ == '__main__':
+    stores = Store()
+
+    print(stores.credential_store.get_state())
+
+    stores.credential_store.dispatch({'type': 'safe_to_disk'})
