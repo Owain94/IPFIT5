361e8c15ebc2bed9e12a19744599da25d7317ecf
Owain van Brakel <owain.vanbrakel@gmail.com>
2018-03-07 21:28:29 +0100

    Add logging store

---
+++ Utils/Logging/Store/Actions/LoggingActions.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from typing import Dict, Union
+
+
+class LoggingStoreActions:
+    ADD_LOG = 'ADD_LOG'
+    SAVE_TO_DISK = 'SAVE_TO_DISK'
+
+    @staticmethod
+    def add_log(what: str,
+                why: str,
+                how: str,
+                result: str)-> \
+            Dict[str, Union[str, Dict[str, Union[datetime, str]]]]:
+        return {
+            'type': LoggingStoreActions.ADD_LOG,
+            'log': {
+                'why': why,
+                'what': what,
+                'how': how,
+                'result': result
+            }
+        }
+
+    @staticmethod
+    def save_to_disk():
+        return {
+            'type': LoggingStoreActions.SAVE_TO_DISK
+        }
