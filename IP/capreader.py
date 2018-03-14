ab2e4b9d7291bfc23d545580c45a7bb76035c7a7
stickler-ci <support@stickler-ci.com>
2018-03-14 13:23:03 +0000

    Fixing style errors.

--- IP/capreader.py
+++ IP/capreader.py
@@ -250,7 +250,7 @@ class PcapReader():
     def read_all(f: str, reader, compare: List[str]) -> set:
         return {(src, dst, prot, stamp) for (src, dst, prot, stamp) in
                 reader.extract_all(f) if any(ip in compare for ip in [src, dst]
-        )}
+                                             )}
 
     def set_compatible(self):
         for f in self.files:
