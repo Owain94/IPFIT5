1cca5cacdd299bd2781e65c55f8bb73fe8f2bcec
Owain van Brakel <owain.vanbrakel@gmail.com>
2018-03-16 04:00:44 +0100

    Return file instead of reference

--- Utils/ImageHandler.py
+++ Utils/ImageHandler.py
@@ -195,7 +195,7 @@ class ImageHandler(metaclass=Singleton):
             fs_object.info.name.name.decode('UTF-8') in ['.', '..']
 
     def single_file(self, partition: int, path: str, filename: str,
-                    hashing: bool = False) -> Union[str, pytsk3.File, None]:
+                    hashing: bool = False) -> Union[str, bytes, None]:
         vol, img = self.get_handle()
         fs, root = None, None
 
@@ -218,7 +218,7 @@ class ImageHandler(metaclass=Singleton):
 
                         if file_name.lower() == filename.lower():
                             return self.hash_file(fs_object) if hashing else \
-                                fs_object
+                                self.read_file(fs_object)
                     except IOError:
                         pass
             except RuntimeError:
@@ -305,6 +305,13 @@ class ImageHandler(metaclass=Singleton):
                 pass
         dirs.pop(-1)
         return data
+
+    @staticmethod
+    def read_file(fs_object: pytsk3.File) -> bytes:
+        offset = 0
+        size = getattr(fs_object.info.meta, "size", 0)
+
+        return fs_object.read_random(offset, size)
 
     @staticmethod
     def hash_file(fs_object: pytsk3.File) -> str:
