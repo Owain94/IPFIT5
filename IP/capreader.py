e39c84099e0e6a1c3f887c94f7ef7dfbd47eeb64
DutchGhost <kasper199914@gmail.com>
2018-04-20 17:06:59 +0200

    fix line too long error

--- IP/capreader.py
+++ IP/capreader.py
@@ -473,8 +473,7 @@ class PcapReader(ModuleInterface):
         }
 
     @Reader.compatible(dpkt=lambda f: dpkt.pcap.Reader(f),
-                       pyshark=lambda f: pyshark.FileCapture(f,
-                                                             keep_packets=False))
+                       pyshark=lambda f: pyshark.FileCapture(f))
     def set_compatible(self, pyshark, dpkt):
         """
         sets self.dpkt_compatible and self.pyshark_compatible
